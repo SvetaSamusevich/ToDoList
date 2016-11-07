4946324ada5688aa111e82bc54a0467476096257
SvetaSamusevich <samusevich2@yandex.by>
2016-11-08 00:35:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+﻿var myApp = angular.module('app', ['ui.router']);
+
+myApp.config(function($stateProvider) {
+	var states = [
+    { 
+      name: 'user', 
+      url: '/user', 
+      templateUrl:'/dataPages/user.html'  
+    },
+    
+    { 
+      name: 'todo', 
+      url: '/todo', 
+      templateUrl:'/dataPages/todo.html' 
+    },
+    
+    { 
+      name: 'meetings', 
+      url: '/meetings', 
+      templateUrl:'/dataPages/meetings.html'     
+    }
+  ]
+  
+  states.forEach(function(state){
+	  $stateProvider.state(state);
+  })
+  
+});
+
---
+++ main_controller/main.controller.js
@@ -0,0 +1,43 @@
+﻿angular
+  .module('app')
+  .controller('ToDoContr',function($scope){
+	// initialize list of todos
+	$scope.todos=[
+	    {done: true, text: 'first'},
+	    {done:false, text:'second'}
+	];
+	// function to add new todo 
+	$scope.addTodo = function(){
+		var newTodo = {
+			done: false,
+			text: $scope.todoText
+		};
+		
+		$scope.todos.push(newTodo);
+		$scope.todoText = '';
+	};
+	//function to delete a todo
+	$scope.removeTodo = function (start){
+		$scope.todos.splice(start, 1);
+	}
+	//function to move an item
+	$scope.move = function(index, direction){
+		
+		if(direction === 'up'){
+			if(index === 0){
+				return;
+			}
+			index = index -1;			
+		}
+		
+		if(direction === 'down'){
+			if(index === $scope.todos.length - 1){
+			return;
+		   }
+		}
+		
+		var todo = $scope.todos[index];
+		$scope.todos.splice(index + 2, 0, todo);
+		$scope.todos.splice(index, 1);
+	}
+})
